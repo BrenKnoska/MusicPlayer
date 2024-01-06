54bc7bce48b1d5b0f613b69f39c5f93e4de1e2a6
Brenda Knoska <belmyris@gmail.com>
2024-01-05 22:28:38 -0300

    Agregué funcionalidad al reproductor en JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//guarda en variable referencia a los elementos del dom
 const playCancion = document.getElementById("play");
 const sigCancion = document.getElementById("forward");
 const atrasCancion = document.getElementById("backward");
@@ -5,47 +6,59 @@ const pausaCancion = document.getElementById("pause");
 const disco = document.getElementsByClassName("Disco")[0];
 const pausaBoton = document.getElementById("pausaBoton");
 
-let reproducir;
-
-listaCanciones = [
+/**
+ *@constant se crea un array anidado que contiene la letra y el nombre de cada cancion
+ */
+const listaCanciones = [
   ["./audio/Se_sentia_fantasia.mp3", "Se sentia fantasia"],
   ["./audio/Destello_Fugaz.mp3", "Destello Fugaz"],
   ["./audio/Distancia_inerte.mp3", "Distancia inerte"],
   ["./audio/Silencio_en_la_tempestad.mp3", "Silencio en la tempestad"],
   ["./audio/Flor_en_el_vacio.mp3", "Flor en el vacío"],
 ];
+//creo una pista de audio que inicia en la posicion 0,0 de mi array anidado
+let numeroPista = 0;
+let reproducir = new Audio(listaCanciones[numeroPista][0]);
 
-function listarCanciones(listaCanciones) {
-  for (let i = 0; i < listaCanciones.length; i++) {
-    for (let j = 0; j < listaCanciones[i].length; j++) {}
+// agrupa las funcionalidades que tienen que ejecutarse, si esta en estado "Play" o en estado "Pause"
+// que es el otro estado posible en los reproductores de musica
+function cambiarEstadoReproductor(estadoActual) {
+  reproducir.pause();
+  if (estadoActual == "play") {
+    disco.classList.add("animacionDisco");
+    btnPlay.classList.add("ocultarElemento");
+    pausaBoton.classList.remove("ocultarElemento");
+    reproducir.play();
+  } else {
+    disco.classList.remove("animacionDisco");
+    btnPlay.classList.remove("ocultarElemento");
+    pausaBoton.classList.add("ocultarElemento");
   }
 }
-
-
+//escucho los eventos de cada boton de mi DOM y uso el cambio de estado cuando corresponda
 playCancion.addEventListener("click", function () {
-  reproducir = new Audio(`./audio/Se_sentia_fantasia.mp3`);
-  reproducir.play();
-  disco.classList.add("animacionDisco");
-  btnPlay.classList.add("ocultarElemento");
-  pausaBoton.classList.remove("ocultarElemento");
+  cambiarEstadoReproductor("play");
 });
 
 pausaBoton.addEventListener("click", function () {
-  reproducir.pause();
-  disco.classList.remove("animacionDisco");
-  btnPlay.classList.remove("ocultarElemento");
-  pausaBoton.classList.add("ocultarElemento");
+  cambiarEstadoReproductor("pause");
 });
 
 sigCancion.addEventListener("click", function () {
-  reproducir.pause();
-  reproducir = new Audio(`./audio/Destello_Fugaz.mp3`);
-  reproducir.sigCancion();
-  disco.classList.add("animacionDisco");
+  cambiarEstadoReproductor("pause");
+  if (numeroPista < listaCanciones.length && numeroPista >= 0) {
+    numeroPista++;
+    reproducir = new Audio(listaCanciones[numeroPista][0]);
+    cambiarEstadoReproductor("play");
+  }
 });
 
-sigCancion.addEventListener("click", function () {
-  reproducir = new Audio(`./audio/Distancia_inerte.mp3`);
-  reproducir.play();
-  disco.classList.add("animacionDisco");
+//Botón reproductor atrás
+atrasCancion.addEventListener("click", function () {
+  cambiarEstadoReproductor("pause");
+  if (numeroPista > 0) {
+    numeroPista--;
+    reproducir = new Audio(listaCanciones[numeroPista][0]);
+    cambiarEstadoReproductor("play");
+  }
 });
